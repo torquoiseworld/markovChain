58a7fa1897ce2ddab37a0f92bb059fac74866940
Valerie Zhang <valeriezhang@Valeries-MBP.wowway.com>
2018-07-03 20:26:44 -0400

    First iteration, txt file not added.

---
+++ markov.py
@@ -0,0 +1,24 @@
+import random
+
+with open('whatWasNeeded.txt','r') as f:
+    wordArray = []
+    wordDict = {}
+    for line in f:
+        for word in line.split():
+           wordArray.append(word)
+for i in range(0, len(wordArray)-3):
+    if (wordArray[i], wordArray[i+1]) in wordDict:
+      wordDict[(wordArray[i], wordArray[i+1])].append(wordArray[i+2])
+    else:
+      wordDict[(wordArray[i], wordArray[i+1])] = [wordArray[i+2]]
+
+randTuple = random.choice(list(wordDict))
+outputArr = [randTuple[0], randTuple[1]]
+for num in range(0, 50):
+  possWordList = wordDict[(outputArr[num], outputArr[num+1])]
+  randIndex = random.randint(0, len(possWordList)-1)
+  outputArr.append(possWordList[randIndex])
+  num += 1
+print(*outputArr, sep= ' ')
+
+

32ac938342f64465529d0d522a72199c1fb7c81f
Valerie Zhang <valeriezhang@Valeries-MBP.wowway.com>
2018-07-03 22:58:11 -0400

    Starts with capital letter now

--- markov.py
+++ markov.py
@@ -2,6 +2,7 @@ import random
 
 with open('whatWasNeeded.txt','r') as f:
     wordArray = []
+    capLetterDict = {}
     wordDict = {}
     for line in f:
         for word in line.split():
@@ -11,14 +12,18 @@ for i in range(0, len(wordArray)-3):
       wordDict[(wordArray[i], wordArray[i+1])].append(wordArray[i+2])
     else:
       wordDict[(wordArray[i], wordArray[i+1])] = [wordArray[i+2]]
+      if (wordArray[i][0].isupper()):
+        capLetterDict[(wordArray[i], wordArray[i+1])] = [wordArray[i+2]]
 
-randTuple = random.choice(list(wordDict))
+
+randTuple = random.choice(list(capLetterDict))
 outputArr = [randTuple[0], randTuple[1]]
-for num in range(0, 50):
+for num in range(0, 100):
   possWordList = wordDict[(outputArr[num], outputArr[num+1])]
   randIndex = random.randint(0, len(possWordList)-1)
   outputArr.append(possWordList[randIndex])
   num += 1
 print(*outputArr, sep= ' ')
+print()
 
 
